e920ace5c2874c953e6fdc9f794196a02a397c48
TheDoink <littletonriggins@gmail.com>
2017-08-19 15:27:30 -0400

    Typing in a users name now gets their data, populates it back to the parent, and displays their pic and username. Also included jquery solely for handling http requests #5

--- src/App.js
+++ src/App.js
@@ -1,16 +1,70 @@
 import React, { Component } from 'react';
 import NameInput from './Components/NameInput'
 
+
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      gitUser: {}
+    }
+  }
+
+  // When one of our children sets the gitUser, set it here
+  handleAddUser = (user) => {
+    console.log("ADDING USER");
+
+    console.log(user);
+    this.setState({gitUser: user}, function() {
+
+    });
+  }
+
+  getUserIconStyle = () => {
+
+    // Define a box where the image will go 
+    let ret = {
+      'width':'30vw',
+      'height':'30vw',
+      'backgroundPosition':'center',
+      'backgroundSize':'contain',
+      'display':'inline-block'
+      // '-moz-border-radius': '15vw',
+      // '-webkit-border-radius': '15vw',
+      // 'border-radius': '30vw'
+    };
+
+    // If we have a user...
+    if(this.state.gitUser.avatar_url) {
+      
+      // Display that user's image
+      ret['backgroundImage'] = `url('${this.state.gitUser.avatar_url}')`;
+    } else {
+
+      // Otherwise hide it
+      ret.display = "none";
+    }
+
+    return ret;
+  }
+
   render() {
+    let showUserIcon = this.state.gitUser.avatar_url ? "none": "visible";
     return (
       <div className="App">
-        <div className="personIcon">
-          <i className="fa fa-user-circle-o"></i>
+        <div id="iconRow">
+          <div id="userIcon" style={this.getUserIconStyle()}></div>
+          <div className="personIcon" style={{display: showUserIcon}}>
+            <i className="fa fa-user"></i>
+          </div>
         </div>
-        <NameInput>
+        <h2>{this.state.gitUser.login}</h2>
+        <NameInput setUser={this.handleAddUser.bind(this)}>
           
         </NameInput>
+
+
       </div>
     );
   }
--- src/Components/NameInput.js
+++ src/Components/NameInput.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './NameInput.css';
+import $ from 'jquery';
 
 class NameInput extends Component {
 
@@ -13,13 +14,13 @@ class NameInput extends Component {
 
   /* Validation for users typing in names */
   handleUpdate = (e) => {
+    e.preventDefault();
+
+    console.log("CHANGING");
+
     let val = e.target.value;
 
-    /* Github regex found from: https://github.com/shinnn/github-username-regex
-       Regex was also validated by github username requirements page */
-    let githubUsernameRegex = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i;
-
-    /* Github has a maxLength of usersnames for 39 chars */
+    /* Github has a maxLength of usernames for 38 chars */
     if(val.length <= 38) {
       this.setState({name: val}, function() {
         console.log(this.state);
@@ -29,19 +30,46 @@ class NameInput extends Component {
 
   handleSubmit = (e) => {
 
+    e.preventDefault();
+    console.log("SUBMITTIN");
+
+    let name = this.state.name;
+
+    /* Github regex found from: https://github.com/shinnn/github-username-regex
+       Regex was also validated by github username requirements page */
+    let githubUsernameRegex = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i;
+
+    // If we pass the regex...
+    if(githubUsernameRegex.test(name)) {
+
+      // Try and get that username...
+      let gitPromise = $.get(`https://api.github.com/users/${name}`,
+
+        // If we get a good response...
+        (success) => {
+          console.log(success);
+          this.props.setUser(success);
+          this.setState({name: ""}, function() {});
+        })
+
+        // If we get anything else...
+        .fail((failure) => {
+          console.log("FAIL");
+        });
+      
+    }
   };
 
   render() {
     
     return (
-      <form id="nameInputForm" >
+      <form id="nameInputForm" onSubmit={this.handleSubmit.bind(this)}>
         <div id="nameInputElement">
           <input id="nameInput" placeholder="Enter A Git Username" onChange={this.handleUpdate.bind(this)} type="text" value={this.state.name}/>
           <span id="submitButton" onClick={this.handleSubmit.bind(this)}>
             <i className="fa fa-arrow-circle-o-right"></i>
           </span>
         </div>
-          
       </form>
     );
   }
